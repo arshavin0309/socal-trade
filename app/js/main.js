c7440d876b6cc6608c9e1a8c5c67ef9110698ea8
Владислав <arshavin0309@mail.ru>
2024-09-26 13:07:55 +0500

    add

--- app/js/main.js
+++ app/js/main.js
@@ -128,15 +128,31 @@ $(document).ready(function () {
 
 });
 
-
 var swiper = new Swiper(".swiper1", {
+    loop: true,
     spaceBetween: 30,
     centeredSlides: true,
 
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
+});
+
+var swiper = new Swiper(".swiper2", {
+    loop: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
 
     navigation: {
         nextEl: ".swiper-button-next",
